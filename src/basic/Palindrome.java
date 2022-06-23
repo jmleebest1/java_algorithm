fe942d3906cc87cb6278484d47f8bb06f64e5953
jmleebest1 <skminy27@nate.com>
2022-06-23 22:52:05 +0900

    팰린드롬

---
+++ src/basic/Palindrome.java
@@ -0,0 +1,16 @@
+//입력된 수가 팰린드롬인지 판별하시오
+//입력: 12321
+//정답: True
+//*팰린드롬: 역순으로 읽어도 같은 말이 되는 것
+package basic;
+
+public class Palindrome {
+	public static void main(String[] args) {
+		String n = "12321";
+		
+		int len = n.length();
+		for(int i = 0; i< len/2; i++) {
+//			if(n.charAt(i) == n.charAt())
+		}
+	}
+}
