caf9f4a67d68af8082329c1c92a15c60e99ea202
jmleebest1 <skminy27@nate.com>
2022-06-26 23:24:23 +0900

    완전수

---
+++ src/codingStudio/PerfectNumber.java
@@ -0,0 +1,26 @@
+package codingStudio;
+
+import java.util.Scanner;
+
+public class PerfectNumber {
+	public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("수 입력 : ");
+        int n = sc.nextInt();
+
+        for(int i=1; i<=n; i++) {
+            int sum = 0;
+            for(int j=1; j<i; j++) {
+                if(i%j==0) {
+                    sum += j;
+                }
+            }
+            if(i==sum) {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
