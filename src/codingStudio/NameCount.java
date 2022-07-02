b934d870d3e144bcb14475e73ea5099adce69e33
jmleebest1 <skminy27@nate.com>
2022-07-02 23:53:00 +0900

    NameCount 2,3번

--- src/codingStudio/NameCount.java
+++ src/codingStudio/NameCount.java
@@ -1,5 +1,7 @@
 package codingStudio;
 
+import java.util.ArrayList;
+
 //이유덕,이재영,권종표,이재영,박민호,강상희,이재영,김지완,최승혁,이성연,박영서,박민호,전경헌,송정환,김재성,이유덕,전경헌
 //1. 김씨와 이씨는 각각 몇 명 인가요?
 //2. "이재영"이란 이름이 몇 번 반복되나요?
@@ -12,11 +14,27 @@ public class NameCount {
 		String[] names = input.split(",");
 		
 		int count = 0;
+		ArrayList<String> name_list = new ArrayList<String>();
 		
 		for(int i=0; i<names.length; i++) {
+			
 		if(names[i].equals("이재영"))
 			count++;
+		
+		if(!name_list.contains(names[i]))
+			name_list.add(names[i]);
 		}
-		 System.out.println(count);
+		
+		System.out.println("이재영 이름 반복 : " + count + "회");
+		
+		String[] name_arr = name_list.toArray(new String[name_list.size()]);
+		
+		System.out.println("중복을 제거한 이름 : ");
+		for(int i = 0; i < name_arr.length; i++) {
+			System.out.print(name_arr[i] + ((name_arr.length == i + 1)?"\n":", "));
+		}
+		
+	
 	}
+	
 }
